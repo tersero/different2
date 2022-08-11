14b59d73a1a3a1c84a2296d46dfc950274ce91fe
tersero <tersero@mail.ru>
2022-08-11 16:59:50 +0300

    cmd-open04 modify

--- cmd_open4.py
+++ cmd_open4.py
@@ -1,12 +1,17 @@
 from subprocess import Popen
 
-
+print(100 * '-')
+print('Программа для автоматического запуска пингования хостов')
 while True:
-    last_digits = input('Введите последние цифры IP адреса сервера, например: 200 (завершить - введите 0) ')
+    print(100 * '-')
+    print('Введите последние цифры IP адреса компьютера или сервера,')
+    last_digits = input('например: 200 (завершить - введите 0): ')
     if last_digits == '0':
+        print(100 * '-')
         print('Программа завершена')
         break
+    print('Старт пинга хоста 192.168.1.' + last_digits)
     full_ip = 'start ping -t 192.168.1.' + last_digits
-    print(full_ip)
+    # print(full_ip)
     p = Popen(full_ip, shell=True)
 
